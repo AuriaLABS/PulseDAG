63351f8a18bd8abb9f596298376ceaffcf5feb1a
kalekói <hermida.prada@gmail.com>
2026-05-23 00:23:40 +0200

    Fix readiness release flag variable

--- crates/pulsedag-rpc/src/handlers/readiness.rs
+++ crates/pulsedag-rpc/src/handlers/readiness.rs
@@ -469,6 +469,8 @@ pub async fn get_readiness<S: RpcStateLike>(
         );
     }
 
+    let private_testnet_ready = blockers.is_empty();
+
     let warnings = categories
         .iter()
         .filter(|(_, category)| {
